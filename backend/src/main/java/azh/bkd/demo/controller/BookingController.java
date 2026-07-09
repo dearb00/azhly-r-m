a8d648e21d721deab6f6364866ce774ddf1c185a
dearb00 <deadbeal72@gmail.com>
2026-07-09 21:57:57 +0000

    ....

--- backend/src/main/java/azh/bkd/demo/controller/BookingController.java
+++ backend/src/main/java/azh/bkd/demo/controller/BookingController.java
@@ -1,7 +1,7 @@
-package com.example.azhly.controller;
+package azh.bkd.demo.controller;
 
-import com.example.azhly.model.RoomBooking;
-import com.example.azhly.repository.RoomBookingRepository;
+import azh.bkd.demo.model.RoomBooking;
+import azh.bkd.demo.repository.RoomBookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
